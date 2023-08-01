482fd6edef94210fc4fe0fc7b5f7829e212b3a6d
Maarten <m.ottenhoff@maastrichtuniversity.nl>
2023-08-01 13:16:52 +0000

    First update on new feature selection method

--- libs/feature_selection/top_correlated.py
+++ libs/feature_selection/top_correlated.py
@@ -17,22 +17,23 @@ def select_top_correlated(y, z, n_features):
 
     corrcoefs = np.abs(np.corrcoef(np.hstack([y, z]), rowvar=False)[-z.shape[-1]:, :y.shape[1]])
 
-    # # # Highest summed correlation
-    # # max_summed_cc = np.abs(corrcoefs[:, :y.shape[1]]).sum(axis=0)
-    # # return np.argsort(max_summed_cc)[-n_features:]  
 
-    # Sort each dimension seperately
-    # sorted_ccs = np.vstack([np.argsort(np.abs(row)) for row in corrcoefs])
-    sorted_ccs = np.argsort(corrcoefs, axis=1)
 
-    # Gets highest for each dim, iteratively
-    features = set()
-    for ccs in sorted_ccs.T[::-1, :]:
-        n_to_add = min((n_features - len(features)), ccs.size)
+    if summed_correlation := False:
+        max_summed_cc = np.abs(corrcoefs[:, :y.shape[1]]).sum(axis=0)
+        logger.info(f'Selected top summer correlation: {np.argsort(max_summed_cc)[-n_features:]}')
+        return np.argsort(max_summed_cc)[-n_features:]
 
-        features = features | set(ccs[:n_to_add])
-        
-        if len(features) == n_features:
-            logger.info(f'selected features (ch_nums): {features}')
-            logger.info('Summed correlation: ' + ' | '.join([f'{c:.2f}' for c in corrcoefs[:, list(features)].sum(axis=0)]))
-            return np.array(list(features))
+    if top_n_per_dim := True:
+        sorted_ccs = np.argsort(corrcoefs, axis=1)
+        # Gets highest for each dim, iteratively
+        features = set()
+        for ccs in sorted_ccs.T[::-1, :]:
+            n_to_add = min((n_features - len(features)), ccs.size)
+
+            features = features | set(ccs[:n_to_add])
+            
+            if len(features) == n_features:
+                logger.info(f'selected features (ch_nums): {features}')
+                logger.info('Summed correlation: ' + ' | '.join([f'{c:.2f}' for c in corrcoefs[:, list(features)].sum(axis=0)]))
+                return np.array(list(features))
