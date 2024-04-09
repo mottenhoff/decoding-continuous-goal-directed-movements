69a87e133335acb2303c3de0e5db5de927bd4cde
Ottenhoff <P70066129@unimaas.nl>
2024-04-09 10:38:13 +0200

    parallelized and matrix plot

--- transfer_learning.py
+++ transfer_learning.py
@@ -18,7 +18,8 @@ kinematics:
 import pickle
 from pathlib import Path
 from copy import deepcopy
-from itertools import combinations
+from itertools import combinations, product
+from multiprocessing import Pool
 
 # import matplotlib
 # matplotlib.use('Qt5Agg')
@@ -30,6 +31,10 @@ from PSID.evaluation import evalPrediction as eval_prediction
 N_KINEMATICS = 12
 N_FOLDS = 5
 MEASURE = 'CC'
+
+A_TO_B = 2
+B_TO_A = 3
+SPEED = 10
 
 def cv_split(y, z, n_folds):
     # Test size = 1/n_folds
@@ -58,19 +63,22 @@ def train_baseline(y, z):
 
     return baseline
 
+def get_pair_names(pairs):
+    return [(pair_a.name, pair_b.name) for pair_a, pair_b in pairs]
+
 def highest_pairs(results, pairs):
     # Do higher source and/or target scores lead to higher
     KINEMATIC_SPEED = 10
     A_TO_B = 2
     B_TO_A = 3
 
-    pair_names = [(pair_a.name, pair_b.name) for pair_a, pair_b in pairs]
+    pair_names = get_pair_names(pairs)
     
-    results_mean = results.mean(axis=2)
+    results_mean = np.abs(results.mean(axis=2))
 
     highest_pair_score = results_mean[:, :2, KINEMATIC_SPEED].sum(axis=1)
     
-    fig, ax = plt.subplots(nrows=2, ncols=2)
+    fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(12, 8))
     ax[0, 0].scatter(highest_pair_score, results_mean[:, A_TO_B, KINEMATIC_SPEED])
     ax[0, 0].set_xlabel('summed_individual_score')
     ax[0, 0].set_ylabel('A to B performance')
@@ -87,13 +95,76 @@ def highest_pairs(results, pairs):
     ax[1, 1].set_ylabel('decoding performance B')
     ax[1, 1].set_title('size by A to B score')
 
-    fig.savefig('relationship baseline and transferability.png')
-    plt.show()
-
+    fig.savefig(f'relationship_baseline_and_transferability_{pairs[0][0].parent.name}.png')
     return
 
 
-def transfer_learn(y_a, y_b, z_a, z_b):
+def plot_matrix(results, pairs, savepath):
+
+    results = results.mean(axis=2)
+
+    pair_names = np.array(get_pair_names(pairs))
+    ppts = np.unique(pair_names)
+    mapping = dict(zip(ppts, np.arange(ppts.size)))
+
+    matrix = np.zeros((ppts.size, ppts.size))
+
+    for idx, (a, b) in enumerate(pair_names):
+        matrix[mapping[a], mapping[b]] = results[idx, A_TO_B, SPEED]
+        matrix[mapping[b], mapping[a]] = results[idx, B_TO_A, SPEED]
+
+    # # TODO: add diagonal of within score
+
+    fig, ax = plt.subplots(figsize=(12, 12))
+
+    # matrix = matrix.T
+
+    print(results[:3, 2, 10], results[:3, 3, 10])
+
+
+    # ax.imshow(matrix, cmap='plasma', vmin=0, vmax=1)
+    color_boundary = np.abs(matrix).max()
+    ax.imshow(matrix, cmap='coolwarm', vmin=-color_boundary, vmax=color_boundary)
+
+    for s, t in product(np.arange(matrix.shape[0]), np.arange(matrix.shape[1])):
+        ax.annotate(f'{matrix[s, t]:0.2f}', (s, t), ha='center', va='center', color='w')
+
+    ax.set_xlabel('Source', fontsize='xx-large')
+    ax.set_ylabel('Target', fontsize='xx-large')
+    ax.set_xticks(np.arange(len(ppts)))
+    ax.set_yticks(np.arange(len(ppts)))
+    ax.set_yticklabels(ppts)
+    ax.set_xticklabels(ppts)
+    ax.invert_yaxis()
+
+    fig.suptitle(f'{pairs[0][0].parent.name}', fontsize='xx-large')
+    fig.savefig(f'figure_output/transfer_learning/transfer_learning_matrix_{pairs[0][0].parent.name}.png')
+
+    return ax
+    
+
+def read_results(path, pairs):
+    files = [file for file in path.glob('**/*') if file.is_file()]
+    pairs = np.array([(a.parts[2], b.parts[2]) for a, b in pairs])
+    results = np.empty((len(pairs), 4, N_FOLDS, N_KINEMATICS))
+
+    for file in files:
+        pair_a, pair_b = file.stem.split('_')
+        idx = np.where(np.logical_and(pairs[:, 0] == pair_a, pairs[:, 1] == pair_b))
+        results[idx] = np.load(file)
+
+    return results
+
+
+# def transfer_learn(y_a, y_b, z_a, z_b):
+def transfer_learn(path_a: Path, path_b: Path, savepath: Path):
+
+    print(f'Fitting: {path_a.name} and {path_b.name}', flush=True)
+
+    y_a = np.load(path_a/'0'/'y.npy')
+    z_a = np.load(path_a/'0'/'z.npy')
+    y_b = np.load(path_b/'0'/'y.npy')
+    z_b = np.load(path_b/'0'/'z.npy')
 
     # train_model a for fold i
     # train_model b for fold i
@@ -139,65 +210,70 @@ def transfer_learn(y_a, y_b, z_a, z_b):
         transfer_ab[i, :] = cc_ab
         transfer_ba[i, :] = cc_ba
 
-    return baseline_a, baseline_b, transfer_ab, transfer_ba
+    result = np.stack([baseline_a, baseline_b, transfer_ab, transfer_ba])
+    np.save(savepath/f'{path_a.name}_{path_b.name}.npy', result)
 
+def run_async(pairs, savepath):
+    pool = Pool(processes=8)
+    for path_a, path_b in pairs:
+        pool.apply_async(transfer_learn, args=(path_a, path_b, savepath))
+    pool.close()
+    pool.join()
 
+if __name__=='__main__':
+
+    run_parallel = True
 
 # Negative correlations might be because of the wide variation of correlations in delta, as some are strongly negatively correlated, and others strongly positively.
 #      if so, then this should not be the case for bbhg and or alphabeta. The latter may not have sufficient performane in the first place.
+# --> This is not true, as a similar distribution is found for bbhg, albeit lower.
 
+    # Input data
+    main_path = Path(r'finished_runs\delta')
+    # main_path = Path(r'finished_runs\bbhg')
+    # main_path = Path(r'finished_runs\alphabeta')
 
+    savepath = Path(f'results_transfer_learning/{main_path.name}/pairs')
+    savepath.mkdir(exist_ok=True, parents=True)
 
-path_a = Path(r'results\20240207_1556\20221103\0')
-path_b = Path(r'results\20240207_2001\Bubbles_15_53_18\0')
+    pairs = [(path_a, path_b) for path_a, path_b in combinations(main_path.glob('kh*'), 2)]
 
-main_path = Path(r'finished_runs\delta')
-main_path = Path(r'finished_runs\bbhg')
-# main_path = Path(r'finished_runs\alphabeta')
-pairs = [(path_a, path_b) for path_a, path_b in combinations(main_path.glob('kh*'), 2)]
+    if False:
+        # Rerun transfer learning
+        if run_parallel:
+            run_async(pairs, savepath)
+        else:
+            for path_a, path_b in pairs:
+                transfer_learn(path_a, path_b, savepath)
 
-results = np.empty((0, 4, N_FOLDS, N_KINEMATICS))
-for path_a, path_b in pairs:
-    try:
-        y_a = np.load(path_a/'0'/'y.npy')
-        z_a = np.load(path_a/'0'/'z.npy')
-        y_b = np.load(path_b/'0'/'y.npy')
-        z_b = np.load(path_b/'0'/'z.npy')
-    except FileNotFoundError as e:
-        print(e)
-        print('skipping...')
+    results = read_results(savepath, pairs)
 
-    result_pair = np.stack(transfer_learn(y_a, y_b, z_a, z_b))
-    results = np.vstack([results, np.expand_dims(result_pair, 0)])
-    print('.', end='')
+    plot_matrix(results, pairs, savepath)
 
-np.save('transfer_results.npy', results)
+    highest_pairs(results, pairs)
 
-results = np.load('transfer_results.npy')
+    fig, axs = plt.subplots(nrows=1, ncols=4, figsize=(12, 8))
 
-highest_pairs(results, pairs)
+    for i in range(4):
+        scores = results.mean(axis=2)[:, i, :]
+        idc = np.tile(np.arange(12), scores.shape[0]).reshape((scores.shape[0], -1))
+        axs[i].violinplot(scores, np.arange(N_KINEMATICS), showextrema=False)
+        axs[i].scatter(idc, scores, color='black', marker='o', s=2)
 
-fig, axs = plt.subplots(nrows=1, ncols=4, figsize=(12, 8))
+    axs[0].set_title('Baseline A')
+    axs[1].set_title('Baseline B')
+    axs[2].set_title('Swapped\ninserted Cz_b to model A\nRepredicted y_a')
+    axs[3].set_title('Swapped\ninserted Cz_a to model B')
+    axs[0].set_ylabel('Reconstruction CC')
+    axs[0].set_xlabel('kinematics')
+    fig.suptitle(f'{main_path.name}')
 
-for i in range(4):
-    scores = results.mean(axis=2)[:, i, :]
-    idc = np.tile(np.arange(12), scores.shape[0]).reshape((scores.shape[0], -1))
-    axs[i].violinplot(scores, np.arange(N_KINEMATICS), showextrema=False)
-    axs[i].scatter(idc, scores, color='black', marker='o', s=2)
+    for ax in axs:
+        ax.set_ylim(-1, 1)
+        ax.set_xticks(np.arange(N_KINEMATICS))
 
-axs[0].set_title('Baseline A')
-axs[1].set_title('Baseline B')
-axs[2].set_title('Swapped\ninserted Cz_b to model A\nRepredicted y_a')
-axs[3].set_title('Swapped\ninserted Cz_a to model B')
-axs[0].set_ylabel('Reconstruction CC')
-axs[0].set_xlabel('kinematics')
+    outpath = Path(f'figure_output/transfer_learning')
+    outpath.mkdir(exist_ok=True, parents=True)
+    fig.savefig(outpath/f'transfer_learning_{main_path.name}.png')
 
-for ax in axs:
-    ax.set_ylim(-1, 1)
-    ax.set_xticks(np.arange(N_KINEMATICS))
-
-outpath = Path(f'figure_output/transfer_learning')
-outpath.mkdir(exist_ok=True, parents=True)
-fig.savefig(outpath/'transfer_learning.png')
-
-fig.show()
+    plt.show()
