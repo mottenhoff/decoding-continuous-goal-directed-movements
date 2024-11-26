601714e60ea80356c0361b1e363cc47299ac35ae
mottenhoff <github@ottenhoff.me>
2024-11-26 14:35:38 -0500

    merge

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ import cProfile
 import pstats
 import io
 import re
-from os import cpu_count
 from multiprocessing import Pool
 from pathlib import Path
 from datetime import datetime as dt
@@ -107,7 +106,7 @@ def main():
     main_path.mkdir(parents=True, exist_ok=True)
 
     # data_path = Path(sys.argv[2])
-    data_path = Path(r'../data/')
+    data_path = Path(r'C:\Users\micro\main\resources\data\bubbles-psid-2024')
     filenames = list(data_path.rglob('*.xdf'))  # All xdf files.
 
     # Combine multiple sessions of one participant
@@ -125,7 +124,7 @@ def main():
 
     if c.parallel:
         
-        pool = Pool(processes=cpu_count())
+        pool = Pool(processes=8) #cpu_count())
         for job in jobs:
             pool.apply_async(init_run, args=(job, main_path))
         pool.close()
